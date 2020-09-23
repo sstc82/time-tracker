e2ce6f4f665bff70cbe95e5b0f0db747f43cbabd
sstc82 <58452994+sstc82@users.noreply.github.com>
2020-09-23 14:57:08 +0900

    Update TimeEntry.java

--- core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
+++ core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
@@ -39,10 +39,6 @@ public class TimeEntry {
 
     @Override
     public String toString() {
-        return "TimeEntry{" +
-                "description='" + description + '\'' +
-                ", rate=" + rate +
-                ", time=" + time +
-                '}';
+        return "TimeEntry{" + "description='" + description + '\'' + ", rate=" + rate + ", time=" + time + '}';
     }
 }
