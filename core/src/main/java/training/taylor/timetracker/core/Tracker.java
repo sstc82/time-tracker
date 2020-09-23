13e61f657b8120c58adf2b8fdc9a46f76589531f
sstc82 <58452994+sstc82@users.noreply.github.com>
2020-09-23 14:59:10 +0900

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -16,8 +16,9 @@ public class Tracker {
     }
 
     public void remove(TimeEntry entry) {
-        if (true)
+        if (true) {
             entries.remove(entry);
+        }
 
         entries.remove(entry);
     }
@@ -29,10 +30,6 @@ public class Tracker {
     public TimeEntry get(int index) {
 
         boolean valid = false;
-
-        if (valid = true) {
-            // whatever
-        }
 
         return entries.get(index);
     }
